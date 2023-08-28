9e05e23ac3addfa00585e6121b0365064aa72452
JonZudell <jonathon.zudell@gmail.com>
2023-08-28 08:05:47 -0500

    metronome

--- src/components/metronome.tsx
+++ src/components/metronome.tsx
@@ -1,44 +1,57 @@
-'use client'
+'use client';
 
 import Script from 'next/script';
 import React, { useState } from 'react';
 import * as Tone from 'tone';
 
-export default function Metronome() {
+const synth = new Tone.Synth().toDestination();
 
-  const [deltaTime, setDeltaTime] = useState(Tone.now());
-  const [lastTime, setLastTime] = useState(0);
-  const [synth, setSynth] = useState(new Tone.Synth().toDestination());
-
-  const [loop, setLoop] = useState(new Tone.Loop((time) => {
-    synth.triggerAttackRelease("C4", "16n", time + 0.1);
-    setDeltaTime(time);
-    setLastTime(time);
-    console.log('deltaTime: %s, lastTime: %s, Tone.now(): %s', deltaTime,  lastTime, Tone.now())
-  }, "4n"));
-  
-  const reset = () => {
-    setLoop(new Tone.Loop((time) => {
-      synth.triggerAttackRelease("C4", "16n", time + 0.1);
-      setDeltaTime(time - lastTime);
-      setLastTime(time);
-      console.log('time: %s,deltaTime: %s, lastTime: %s, Tone.now(): %s', time, deltaTime,  lastTime, Tone.now())
-    }, "4n"))
+const iterate = (time: number, loopStep: number, startTime: number, lastTime: number, upBeat: string, downBeat: string, duration: string, rhythmicGranularity: string, setStep: ((loopStep: number) => void)) => {
+  loopStep = Math.round((time - startTime) / Tone.Time(rhythmicGranularity).toSeconds())
+  setStep(loopStep);
+  if (loopStep % 4 == 0 ) {
+    synth.triggerAttackRelease(downBeat, duration, time);
+  } else {
+    synth.triggerAttackRelease(upBeat, duration, time);
   }
+  console.log('loopStep: %s, dt: %s, totalTime: %s, time: %s', loopStep, time - lastTime, time - startTime, time);
+  lastTime = time;
+}
+
+export default function Metronome() {
+  var lastTime = 0;
+  var startTime = 0;
+  var loopStep = -1;
+  const [step, setStep] = useState(loopStep);
+  const [downBeat, setDownbeat] = useState("C4");
+  const [upBeat, setUpbeat] = useState("C3");
+  const [duration, setDuration] = useState("32n");
+  const [rhythmicGranularity,  setRhythmicGranularity] = useState("4n");
+  
+  const [loop, setLoop] = useState(new Tone.Loop((time) => iterate(time, loopStep, startTime, lastTime, upBeat, downBeat, duration, rhythmicGranularity, setStep), rhythmicGranularity));
   
   const play = (event: React.MouseEvent<HTMLButtonElement>) => {
     event.preventDefault();
+    lastTime = Tone.now();
+    startTime = lastTime;
     Tone.start()
-    Tone.Transport.start(Tone.now());
+    Tone.Transport.start();
     loop.start();
   };
   
   const stop = (event: React.MouseEvent<HTMLButtonElement>) => {
     event.preventDefault();
-    Tone.Transport.stop(Tone.now());
-    loop.stop();
-    reset();
+    loop.stop()
+    Tone.Transport.stop();
   };
+
+  React.useEffect(() => {
+    return () => {
+      loop.stop()
+      Tone.Transport.stop();
+    };
+  }, []);
+
   return (
     <>
       <div onLoad={() => {
@@ -47,18 +60,18 @@ export default function Metronome() {
       } className='bg-gray-900 text-center'>
         <h3 className='text-lg font-semibold tracking-wider text-pink-400'>Metronome</h3>
         <div className='items-center rounded-md py-12'>
-          <input id="0" type="radio" value="false" className='bg-pink-400'></input>
-          <input id="1" type="radio" value="false" className='shadow-lg bg-pink-400'></input>
-          <input id="2" type="radio" value="false" className='bg-pink-400'></input>
-          <input id="3" type="radio" value="false" className='shadow-lg bg-pink-400'></input>
+          <span id="0" className={'relative px-2 mx-1 rounded-md shadow-lg shadow-lg ' + (step % 4 == 0 ? "bg-pink-400" : "bg-pink-700")}></span>
+          <span id="1" className={'relative px-2 mx-1 rounded-md shadow-lg bg-pink-400 shadow-lg ' + (step % 4 == 1 ? "bg-pink-400" : "bg-pink-700")}></span>
+          <span id="2" className={'relative px-2 mx-1 rounded-md shadow-lg bg-pink-400 shadow-lg ' + (step % 4 == 2 ? "bg-pink-400" : "bg-pink-700")}></span>
+          <span id="3" className={'relative px-2 mx-1 rounded-md shadow-lg bg-pink-400 shadow-lg ' + (step % 4 == 3 ? "bg-pink-400" : "bg-pink-700")}></span>
         </div>
         <div className='items-center px-4 py-2 rounded-md'>
-          <div className='relative mx-auto inline-flex items-center px-4 py-2 rounded-md shadow-lg bg-pink-400 hover:bg-pink-700 shadow-lg'>
-            <span onClick={play} id="play" className='text-white font-bold'>Play</span>
-          </div>
-          <div className='relative mx-auto inline-flex items-center px-4 py-2 rounded-md shadow-lg bg-pink-400 hover:bg-pink-700 shadow-lg'>
-            <span onClick={stop} id="stop" className='text-white font-bold'>Stop</span>
-          </div>
+          <span onClick={play} className='relative mx-auto inline-flex items-center px-4 py-2 mx-2 rounded-md shadow-lg bg-pink-400 hover:bg-pink-700 shadow-lg'>
+            <span id="play" className='text-white font-bold'>Play</span>
+          </span>
+          <span  onClick={stop} className='relative mx-auto inline-flex items-center px-4 py-2 mx-2 rounded-md shadow-lg bg-pink-400 hover:bg-pink-700 shadow-lg'>
+            <span id="stop" className='text-white font-bold'>Reset</span>
+          </span>
         </div>
       </div>
     </>
