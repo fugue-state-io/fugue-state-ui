cb6b782b7f64eca11c92887c3df01d53c78a77c2
Jon Zudell <jonathon.zudell@gmail.com>
2023-09-24 22:57:25 +0000

    it's not plagiarism if it's open source

---
+++ src/components/AudioVisualizer/AudioVisualizer.tsx
@@ -0,0 +1,187 @@
+import {
+  useRef,
+  useState,
+  forwardRef,
+  type ForwardedRef,
+  type ForwardRefExoticComponent,
+  type RefAttributes,
+  useImperativeHandle,
+  useEffect,
+} from "react";
+import { type dataPoint } from "./types";
+import { calculateBarData, draw } from "./utils";
+
+interface Props {
+  /**
+   * Audio blob to visualize
+   */
+  blob: Blob;
+  /**
+   * Width of the visualizer
+   */
+  width: number;
+  /**
+   * Height of the visualizer
+   */
+  height: number;
+  /**
+   * Width of each individual bar in the visualization. Default: `2`
+   */
+  barWidth?: number;
+  /**
+   * Gap between each bar in the visualization. Default: `1`
+   */
+  gap?: number;
+  /**
+   * BackgroundColor for the visualization: Default: `"transparent"`
+   */
+  backgroundColor?: string;
+  /**
+   * Color for the bars that have not yet been played: Default: `"rgb(184, 184, 184)""`
+   */
+  barColor?: string;
+  /**
+   * Color for the bars that have been played: Default: `"rgb(160, 198, 255)""`
+   */
+  barPlayedColor?: string;
+  /**
+   * Current time stamp till which the audio blob has been played.
+   * Visualized bars that fall before the current time will have `barPlayerColor`, while that ones that fall after will have `barColor`
+   */
+  currentTime?: number;
+  /**
+   * Custome styles that can be passed to the visualization canvas
+   */
+  style?: React.CSSProperties;
+  /**
+   * A `ForwardedRef` for the `HTMLCanvasElement`
+   */
+  ref?: React.ForwardedRef<HTMLCanvasElement>;
+}
+
+const AudioVisualizer: ForwardRefExoticComponent<
+  Props & RefAttributes<HTMLCanvasElement>
+> = forwardRef(
+  (
+    {
+      blob,
+      width,
+      height,
+      barWidth = 2,
+      gap = 1,
+      currentTime,
+      style,
+      backgroundColor = "transparent",
+      barColor = "rgb(184, 184, 184)",
+      barPlayedColor = "rgb(160, 198, 255)",
+    }: Props,
+    ref?: ForwardedRef<HTMLCanvasElement>
+  ) => {
+    const canvasRef = useRef<HTMLCanvasElement>(null);
+    const [data, setData] = useState<dataPoint[]>([]);
+    const [duration, setDuration] = useState<number>(0);
+
+    useImperativeHandle<HTMLCanvasElement | null, HTMLCanvasElement | null>(
+      ref,
+      () => canvasRef.current,
+      []
+    );
+
+    useEffect(() => {
+      const processBlob = async (): Promise<void> => {
+        if (!canvasRef.current) return;
+
+        if (!blob) {
+          const barsData = Array.from({ length: 100 }, () => ({
+            max: 0,
+            min: 0,
+          }));
+          draw(
+            barsData,
+            canvasRef.current,
+            barWidth,
+            gap,
+            backgroundColor,
+            barColor,
+            barPlayedColor
+          );
+          return;
+        }
+
+        const audioBuffer = await blob.arrayBuffer();
+        const audioContext = new AudioContext();
+        await audioContext.decodeAudioData(audioBuffer, (buffer) => {
+          if (!canvasRef.current) return;
+          setDuration(buffer.duration);
+          const barsData = calculateBarData(
+            buffer,
+            height,
+            width,
+            barWidth,
+            gap
+          );
+          setData(barsData);
+          draw(
+            barsData,
+            canvasRef.current,
+            barWidth,
+            gap,
+            backgroundColor,
+            barColor,
+            barPlayedColor
+          );
+        });
+      };
+
+      processBlob();
+    }, [blob, canvasRef.current]);
+    useEffect(() => {
+      const observer = new ResizeObserver((entries) => {
+        draw(
+          data,
+          canvasRef.current,
+          barWidth,
+          gap,
+          backgroundColor,
+          barColor,
+          barPlayedColor
+        );
+      });
+      observer.observe(document.documentElement);
+      return () => {
+        observer.unobserve(document.documentElement);
+      };
+    }, []);
+
+    useEffect(() => {
+      if (!canvasRef.current) return;
+
+      draw(
+        data,
+        canvasRef.current,
+        barWidth,
+        gap,
+        backgroundColor,
+        barColor,
+        barPlayedColor,
+        currentTime,
+        duration
+      );
+    }, [currentTime, duration]);
+
+    return (
+      <canvas
+        ref={canvasRef}
+        width={width}
+        height={height}
+        style={{
+          ...style,
+        }}
+      />
+    );
+  }
+);
+
+AudioVisualizer.displayName = "AudioVisualizer";
+
+export { AudioVisualizer };
--- src/components/playback_engine.tsx
+++ src/components/playback_engine.tsx
@@ -1,10 +1,26 @@
 'use client';
 import React, { useState, useEffect } from 'react';
 import { FileUploader } from "react-drag-drop-files";
+import { AudioVisualizer } from './AudioVisualizer';
 const fileTypes = ["MP3", "WAV", "FLAC"];
 
 export default function PlaybackEngine() {
   const [file, setFile] = useState(null);
+  const [width, setWidth] = useState(1280);
+  const [height, setHeight] = useState(0);
+
+  useEffect(() => {
+    const observer = new ResizeObserver((entries) => {
+      const rect = entries[0].contentRect;
+      setWidth(rect.width);
+      setHeight(rect.height);
+    });
+    observer.observe(document.documentElement);
+    return () => {
+      observer.unobserve(document.documentElement);
+    };
+  }, []);
+
   const handleChange = (file: React.SetStateAction<null>) => {
     setFile(file);
     console.log(file)
@@ -13,6 +29,15 @@ export default function PlaybackEngine() {
     <div className='bg-gray-900 text-center py-4'>
       <h3 className='text-lg font-semibold tracking-wider text-green-400'>Playback Engine</h3>
       <div className='mx-auto max-w-md'><FileUploader handleChange={handleChange} name="file" types={fileTypes} /></div>
+      <AudioVisualizer
+          id="canvas"
+          blob={file}
+          width={width}
+          height={300}
+          barWidth={1}
+          gap={0}
+          barColor={'#16A34A'}
+        />
     </div>
   )
 }
