74406c7b0092c42e6f2ea0edd4f08fdb71a29ae1
Jon Zudell <jonathon.zudell@gmail.com>
2023-09-24 18:57:45 +0000

    file upload

--- src/components/playback_engine.tsx
+++ src/components/playback_engine.tsx
@@ -1,9 +1,16 @@
 'use client';
 import React, { useState, useEffect } from 'react';
-export default function PlaybackEngine() {
+import { FileUploader } from "react-drag-drop-files";
+const fileTypes = ["MP3", "WAV", "FLAC"];
 
+export default function PlaybackEngine() {
+  const [file, setFile] = useState(null);
+  const handleChange = (file: React.SetStateAction<null>) => {
+    setFile(file);
+  };
   return (
     <div className='bg-gray-900 text-center'>
+      <FileUploader handleChange={handleChange} name="file" types={fileTypes} />
       <h3 className='text-lg font-semibold tracking-wider text-green-400'>Playback Engine</h3>
     </div>
   )
