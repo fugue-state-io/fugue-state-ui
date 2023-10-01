51b78a7072c65491920b9068f27ea64bf30da4cd
Jon Zudell <jonathon.zudell@gmail.com>
2023-10-01 17:59:21 +0000

    metronome time signature programmable

--- src/components/metronome.tsx
+++ src/components/metronome.tsx
@@ -7,8 +7,17 @@ import * as Tone from 'tone';
 export default function Metronome() {
   
   const [playing, setPlaying] = useState(false);
-  const [step, setStep] = useState(-1);
+  const [timeSignature, setTimeSignature] = useState(4);
+  const [step, setStep] = useState(0);
   var synth: Tone.Synth | null = null;
+
+  const getTime = (timeSignature: number) => {
+    let content = [];
+    for (let i = 0; i < timeSignature; i++) {
+      content.push(<span id={i.toString()} className={'relative px-2 mx-1 rounded-md shadow-lg shadow-lg ' + (step % timeSignature == i ? "bg-pink-400" : "bg-pink-700")}></span>);
+    }
+    return content;
+  };
 
   useEffect(() => {
     return () => {
@@ -23,16 +32,16 @@ export default function Metronome() {
       synth = new Tone.Synth();
 
       Tone.Transport.scheduleRepeat((time: number) => {
-        const nextStep = Math.round(Tone.Transport.seconds / Tone.Time("8n").toSeconds());
+        const nextStep = Math.round(Tone.Transport.seconds / Tone.Time(timeSignature + "n").toSeconds());
         if (synth) {
-          if (nextStep % 8 == 0) {
+          if (nextStep % timeSignature == 0) {
             synth.triggerAttackRelease("C4", "32n", time);
           } else {
             synth.triggerAttackRelease("C3", "32n", time);
           }
         }
         setStep(nextStep);
-      }, "8n");
+      }, timeSignature + "n");
 
       synth.toDestination();
     } else {
@@ -47,14 +56,15 @@ export default function Metronome() {
       <div className='bg-gray-900 text-center py-4'>
         <h3 className='text-lg font-semibold tracking-wider text-pink-400'>Metronome</h3>
         <div className='items-center rounded-md py-12'>
-          <span id="0" className={'relative px-2 mx-1 rounded-md shadow-lg shadow-lg ' + (step % 8 == 0 ? "bg-pink-400" : "bg-pink-700")}></span>
-          <span id="1" className={'relative px-2 mx-1 rounded-md shadow-lg bg-pink-400 shadow-lg ' + (step % 8 == 1 ? "bg-pink-400" : "bg-pink-700")}></span>
-          <span id="2" className={'relative px-2 mx-1 rounded-md shadow-lg bg-pink-400 shadow-lg ' + (step % 8 == 2 ? "bg-pink-400" : "bg-pink-700")}></span>
+          {getTime(timeSignature)}
+          {/* <span id="0" className={'relative px-2 mx-1 rounded-md shadow-lg shadow-lg ' + (step % 8 == 0 ? "bg-pink-400" : "bg-pink-700")}></span>
+          <span id="1" className={'relative px-2 mx-1 rounded-md shadow-lg shadow-lg ' + (step % 8 == 1 ? "bg-pink-400" : "bg-pink-700")}></span>
+          <span id="2" className={'relative px-2 mx-1 rounded-md shadow-lg shadow-lg ' + (step % 8 == 2 ? "bg-pink-400" : "bg-pink-700")}></span>
           <span id="3" className={'relative px-2 mx-1 rounded-md shadow-lg bg-pink-400 shadow-lg ' + (step % 8 == 3 ? "bg-pink-400" : "bg-pink-700")}></span>
           <span id="4" className={'relative px-2 mx-1 rounded-md shadow-lg bg-pink-400 shadow-lg ' + (step % 8 == 4 ? "bg-pink-400" : "bg-pink-700")}></span>
           <span id="5" className={'relative px-2 mx-1 rounded-md shadow-lg bg-pink-400 shadow-lg ' + (step % 8 == 5 ? "bg-pink-400" : "bg-pink-700")}></span>
           <span id="6" className={'relative px-2 mx-1 rounded-md shadow-lg bg-pink-400 shadow-lg ' + (step % 8 == 6 ? "bg-pink-400" : "bg-pink-700")}></span>
-          <span id="7" className={'relative px-2 mx-1 rounded-md shadow-lg bg-pink-400 shadow-lg ' + (step % 8 == 7 ? "bg-pink-400" : "bg-pink-700")}></span>
+          <span id="7" className={'relative px-2 mx-1 rounded-md shadow-lg bg-pink-400 shadow-lg ' + (step % 8 == 7 ? "bg-pink-400" : "bg-pink-700")}></span> */}
         </div>
         <div className='items-center px-4 py-2 rounded-md'>
           <span onClick={() => setPlaying(!playing)} className='relative mx-auto inline-flex items-center px-4 py-2 mx-2 rounded-md shadow-lg bg-pink-400 hover:bg-pink-700 shadow-lg'>
