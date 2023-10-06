1bc7b03cda11bc9c517ad9b776b35785fbd65893
Jon Zudell <jonathon.zudell@gmail.com>
2023-10-06 02:27:30 +0000

    ci.sh

--- ci.sh
+++ ci.sh
@@ -5,5 +5,5 @@ export WORKING_DIRECTORY=$(dirname $BOOTSTRAP_PATH)
 export REF=$(git rev-parse HEAD)
 echo ${REF:0:8}
 
-docker build . -t $REGISTRY:${REF:0:8}
-docker push $REGISTRY:${REF:0:8}
+docker build . -t registry.digitalocean.com/fugue-state-registry/fugue-state-ui:${REF:0:8}
+docker push registry.digitalocean.com/fugue-state-registry/fugue-state-ui:${REF:0:8}
