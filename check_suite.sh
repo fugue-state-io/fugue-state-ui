55939d7a4d222ab43a9e664df7334682c0b9a885
Jon Zudell <jonathon.zudell@gmail.com>
2023-10-07 01:55:24 +0000

    ci.json for image builds

--- check_suite.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-export BOOTSTRAP_PATH=$(realpath "$0")
-export WORKING_DIRECTORY=$(dirname $BOOTSTRAP_PATH)
-
-npm run lint
--- ci.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-export BOOTSTRAP_PATH=$(realpath "$0")
-export WORKING_DIRECTORY=$(dirname $BOOTSTRAP_PATH)
-
-export REF=$(git rev-parse HEAD)
-echo ${REF:0:8}
-
-docker build . -t registry.digitalocean.com/fugue-state-registry/fugue-state-ui:${REF:0:8}
-docker push registry.digitalocean.com/fugue-state-registry/fugue-state-ui:${REF:0:8}
