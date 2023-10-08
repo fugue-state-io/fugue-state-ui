dfbad4d952fc4eff8d156a199dce552934d55735
Jon Zudell <jonathon.zudell@gmail.com>
2023-10-08 22:35:20 +0000

    metronome viz

--- src/components/MetronomeVisualizer.tsx
+++ src/components/MetronomeVisualizer.tsx
@@ -4,10 +4,13 @@ import Script from 'next/script';
 import React, { useState, useEffect, useRef } from 'react';
 import * as Tone from 'tone';
 
-export default function MetronomeVisualizer() {
+export default function MetronomeVisualizer(props: {
+  width: string | number | undefined
+  height: string | number | undefined
+}) {
   const canvasRef = useRef<HTMLCanvasElement>(null);
 
   return (
-    <canvas ref={canvasRef}></canvas>
+    <canvas ref={canvasRef} width={props.width} height={props.height}></canvas>
   )
 }
