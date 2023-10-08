7df67a9e9acc16894a87fd5655d9dad57161d36f
Jon Zudell <jonathon.zudell@gmail.com>
2023-10-08 14:51:17 +0000

    upbeat switch

--- src/components/Metronome.tsx
+++ src/components/Metronome.tsx
@@ -8,6 +8,7 @@ import * as Tone from 'tone';
 export default function Metronome() {
   
   const [playing, setPlaying] = useState(false);
+  const [upBeat, setUpBeat] = useState(false);
   const [timeSignature, setTimeSignature] = useState(4);
   const debouncedTimeSignature = useDebounce(timeSignature, 300);
   const [bpm, setBpm] = useState(60);
@@ -23,7 +24,7 @@ export default function Metronome() {
     const loop = new Tone.Loop((time: number) => {
       const nextStep = Math.round(time / Tone.Time(debouncedTimeSignature + "n").toSeconds());
       if (synth) {
-        if (nextStep % debouncedTimeSignature == 0) {
+        if (nextStep % debouncedTimeSignature == 0 && upBeat) {
           synth.triggerAttackRelease("C4", "32n", time);
         } else {
           synth.triggerAttackRelease("C3", "32n", time);
@@ -76,7 +77,7 @@ export default function Metronome() {
         <div className='items-center rounded-md py-12'>
           {getTime(timeSignature)}
         </div>
-        <div className='max-w-md grid grid-cols-2 text-center mx-auto relative my-2'>
+        <div className='max-w-md grid grid-cols-3 text-center mx-auto relative my-2'>
           <div className='flow-root grid-cols-1 px-1 leading-none align-middle'>
             <label htmlFor="bpm" className="block text-sm font-medium leading-6 text-gray-400">
               BPM
@@ -88,6 +89,15 @@ export default function Metronome() {
               Subdivisions
             </label>
             <input name="timeSignature" id="timeSignature"  onChange={event => setTimeSignature(parseInt(event?.target.value))} type="number" value={timeSignature} pattern='\d+'  disabled={playing} className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 px-1"/>
+          </div>
+          <div className='flow-root grid-cols-1 px-1 leading-none align-middle'>
+            <label htmlFor="checkbox" className="block text-sm font-medium leading-6 text-gray-400">
+              Up Beat
+            </label>
+            <label className="relative inline-flex items-center cursor-pointer my-2">
+              <input type="checkbox" checked={upBeat} className="sr-only peer" onChange={() => setUpBeat(!upBeat)} disabled={playing}/>
+              <div className="w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+            </label>
           </div>
         </div>
         <div className='items-center px-4 py-2 rounded-md'>
