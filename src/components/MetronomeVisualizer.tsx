e486ac9860c9b1131dfb938eb84970e6493cd534
Jon Zudell <jonathon.zudell@gmail.com>
2023-10-09 02:14:10 +0000

    Metronome Visualizer

--- src/components/MetronomeVisualizer.tsx
+++ src/components/MetronomeVisualizer.tsx
@@ -5,8 +5,7 @@ import React, { useState, useEffect, useRef } from 'react';
 import * as Tone from 'tone';
 
 export const draw = (
-  zoom: boolean = false,
-  canvas: HTMLCanvasElement,
+  canvas: HTMLCanvasElement | null,
   bpm: number,
   subDivisions: number,
   duration:number
@@ -22,17 +21,19 @@ export default function MetronomeVisualizer(props: {
   width: string | number | undefined
   height: string | number | undefined
   zoom: number [],
-  bpm: number [],
+  bpm: number,
   subDivisions : number,
+  duration: number,
   style?: React.CSSProperties
 }) {
   const canvasRef = useRef<HTMLCanvasElement>(null);
   useEffect(() => {
-
     return () => {
-      
     }
-  },[])
+  },[]);
+  useEffect(() => {
+    draw(canvasRef.current, props.bpm, props.subDivisions, props.duration)
+  },[props.width, props.height, props.bpm, props.subDivisions])
   return (
     <canvas ref={canvasRef} width={props.width} height={props.height}></canvas>
   )
