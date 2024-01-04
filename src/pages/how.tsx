74b158f213a90ce2ec63de456ae0d9a3b16fdbe4
Jon Zudell <jonathon.zudell@gmail.com>
2024-01-04 21:33:32 +0000

    how

--- src/pages/how.tsx
+++ src/pages/how.tsx
@@ -6,11 +6,23 @@ export default function How() {
     <main className="">
       <Navigation />
       <GitHubForkRibbon
-          href="https://github.com/fugue-state-io/fugue-state-ui"
-          position="right"
-          color="black"
-        >Fork me on Github!</GitHubForkRibbon>
-        Coming Soon...
+        href="https://github.com/fugue-state-io/fugue-state-ui"
+        position="right"
+        color="black"
+      >
+        Fork me on Github!
+      </GitHubForkRibbon>
+      <div className="h-screen flex justify-center items-center">
+        <iframe
+          width="1024"
+          height="720"
+          src="https://www.youtube.com/embed/adYPZUH1kSc?si=wZcjKTCER-aTeSfj"
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowFullScreen
+        ></iframe>
+      </div>
     </main>
   );
 }
